ac8b140be38d9c53c97ab691b6588f4045573178
Alighasemi98 <130227966+Alighasemi98@users.noreply.github.com>
2024-12-08 21:42:05 +0330

    Add files via upload

---
+++ CircularLinkedList/CircularLinkedList/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CircularLinkedList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            resources.ApplyResources(this.txtValue, "txtValue");
+            this.txtValue.Name = "txtValue";
+            // 
+            // btnRemove
+            // 
+            resources.ApplyResources(this.btnRemove, "btnRemove");
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click_1);
+            // 
+            // btnAdd
+            // 
+            resources.ApplyResources(this.btnAdd, "btnAdd");
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            resources.ApplyResources(this.lstDisplay, "lstDisplay");
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.SelectedIndexChanged += new System.EventHandler(this.lstDisplay_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtValue);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstDisplay;
+    }
+}
+
---
+++ CircularLinkedList/CircularLinkedList/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircularLinkedList
+{
+    public partial class Form1 : Form
+    {
+        CircularLinkedList<int> list = new CircularLinkedList<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtValue.Text, out int value))
+            {
+                list.Add(value);
+                txtValue.Clear();
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+        private void btnRemove_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtValue.Text, out int value))
+            {
+                list.Remove(value);
+                txtValue.Clear();
+                DisplayList();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+
+        }
+
+        private void DisplayList()
+        {
+            lstDisplay.Items.Clear();
+            foreach (var item in list)
+            {
+                lstDisplay.Items.Add(item);
+            }
+        }
+
+        private void lstDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class CircularLinkedList<T> : System.Collections.Generic.IEnumerable<T> where T : IComparable<T>
+    {
+        private Node head;
+
+        private class Node
+        {
+            public T Value;
+            public Node Next;
+
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        public void Add(T value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+                head.Next = head;
+            }
+            else if (head.Value.CompareTo(value) >= 0)
+            {
+                Node temp = head;
+                while (temp.Next != head)
+                {
+                    temp = temp.Next;
+                }
+                newNode.Next = head;
+                head = newNode;
+                temp.Next = head;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != head && current.Next.Value.CompareTo(value) < 0)
+                {
+                    current = current.Next;
+                }
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+        }
+
+        public void Remove(T value)
+        {
+            if (head == null) return;
+
+            if (head.Value.Equals(value))
+            {
+                if (head.Next == head)
+                {
+                    head = null;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.Next != head)
+                    {
+                        temp = temp.Next;
+                    }
+                    head = head.Next;
+                    temp.Next = head;
+                }
+                return;
+            }
+
+            Node current = head;
+            Node previous = null;
+            do
+            {
+                previous = current;
+                current = current.Next;
+
+                if (current.Value.Equals(value))
+                {
+                    previous.Next = current.Next;
+                    return;
+                }
+            } while (current != head);
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            if (head == null) yield break;
+
+            Node current = head;
+            do
+            {
+                yield return current.Value;
+                current = current.Next;
+            } while (current != head);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
